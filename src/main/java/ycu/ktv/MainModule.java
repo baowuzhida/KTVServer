c3d07579bd7657d2a94122792d115badb4e26e05
Featherzhong <longfeatherzhong@gmail.com>
2018-01-07 01:03:04 +0800

    First Commit

---
+++ src/main/java/ycu/ktv/MainModule.java
@@ -0,0 +1,26 @@
+package ycu.ktv;
+
+import org.nutz.dao.Dao;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.loader.json.JsonLoader;
+import ycu.ktv.entity.User;
+
+import javax.sql.DataSource;
+
+public class MainModule {
+
+    public static void main(String[] args) {
+        User u = new User();
+        u.setUser_name("DarkBao");
+        getDao().insert(u);
+    }
+
+    public static Dao getDao() {
+        Ioc ioc = new NutIoc(new JsonLoader("ioc/dao.js"));
+        DataSource ds = ioc.get(DataSource.class);
+        Dao dao = new NutDao(ds); //如果已经定义了dao,那么改成dao = ioc.get(Dao.class);
+        return dao;
+    }
+}
---
+++ src/main/java/ycu/ktv/entity/User.java
@@ -0,0 +1,62 @@
+package ycu.ktv.entity;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table("kt_user")
+public class User {
+    @Id
+    private int id;
+    @Column("kt_user_name")
+    private String user_name;
+
+    @Column("kt_user_password")
+    private String user_password;
+
+    @Column("kt_user_phone")
+    private String user_phone;
+
+    @Column("kt_user_avatar")
+    private String user_avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getUser_phone() {
+        return user_phone;
+    }
+
+    public void setUser_phone(String user_phone) {
+        this.user_phone = user_phone;
+    }
+
+    public String getUser_avatar() {
+        return user_avatar;
+    }
+
+    public void setUser_avatar(String user_avatar) {
+        this.user_avatar = user_avatar;
+    }
+}
