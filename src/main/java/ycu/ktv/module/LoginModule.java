541779738cd8bab974c32aec8d769fe0e1e8dddd
Baowuzhida <641419341@qq.com>
2018-01-07 23:40:28 +0800

    登录改为手机号和密码 注册增加判断号码重复 超时清除数据库中验证码尚未完成。

--- src/main/java/ycu/ktv/module/LoginModule.java
+++ src/main/java/ycu/ktv/module/LoginModule.java
@@ -1,22 +1,28 @@
 package ycu.ktv.module;
 
+import org.nutz.dao.Cnd;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.GET;
 import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.annotation.Param;
+import ycu.ktv.dao.GetDao;
+import ycu.ktv.entity.Code;
 import ycu.ktv.entity.Message;
+import ycu.ktv.entity.User;
 import ycu.ktv.services.SendMessage;
+
+import java.util.List;
 
 public class LoginModule {
 
     @Ok("json")
     @At("/login")
     @GET
-    public Message login(@Param("phone") String phone){
+    public Message login(@Param("phone") String phone,@Param("password")String password){
         Message message = new Message();
-        double code = (Math.random()*9+1)* 100000;
-        Boolean ifsend= SendMessage.SendMessage(phone,(int)code);
-        if(ifsend){
+//        GetDao.getDao().query(User.class, Cnd.where("kt_user_phone", "=", phone).and("kt_user_password","=",password));
+        if(!GetDao.getDao().query(User.class, Cnd.where("kt_user_phone", "=", phone).and("kt_user_password","=",password)).equals(null)){
+
             message.setBody(null);
             message.setMessage("success");
             message.setStatus("1");
