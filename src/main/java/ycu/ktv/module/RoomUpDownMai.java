fa056fd2ddcda32b7e099df62783973daf498bbf
care <pp313102108@qq.com>
2018-01-10 17:12:39 +0800

    aaa

--- src/main/java/ycu/ktv/module/RoomUpDownMai.java
+++ src/main/java/ycu/ktv/module/RoomUpDownMai.java
@@ -24,31 +24,57 @@ public class RoomUpDownMai {
      * 上麦功能：根据各个歌曲参数初始化数据库里的歌曲表，根据room_id在Playlist表里加入一条信息
      */
     @Ok("Json")
-    @At("/singer/create")
+    @At("/room/singer/create")
     @GET
     public Message roomUpM(@Param("room_id") String room_id, @Param("token") String token, @Param("song_id") String song_id, @Param("song_name") String song_name, @Param("song_singer") String song_singer, @Param("song_music_link") String song_music_link, @Param("song_lrc_link") String song_lrc_link) {
 
         Message message = new Message();
-
         Song song = new Song();
-        int ex_room_id = Integer.valueOf(room_id);
-        int ex_song_id = Integer.valueOf(song_id);
-        if (!(dao.query(Song.class, Cnd.where("kt_song_id", "=", song_id)).isEmpty())) {
-            System.out.println("歌曲在数据库中已存在");
-        } else {
-            song.setId(ex_song_id);
-            song.setSong_name(song_name);
-            song.setSong_singer(song_singer);
-            song.setSong_music_link(song_music_link);
-            song.setSong_lrc_link(song_lrc_link);
-            dao.insert(song);
+        String a ="";
+        int ex_room_id = 0;
+        int ex_song_id = 0;
+
+        try {
+            ex_room_id = Integer.valueOf(room_id);
+            ex_song_id = Integer.valueOf(song_id);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            message.setBody(null);
+            message.setMessage("失败：数据转换出错（String转int）");
+            message.setStatus("3");
+            return message;
+        }
+        try {
+            if (!(dao.query(Song.class, Cnd.where("kt_song_id", "=", song_id)).isEmpty())) {
+                System.out.println(a);
+            } else {
+                song.setId(ex_song_id);
+                song.setSong_name(song_name);
+                song.setSong_singer(song_singer);
+                song.setSong_music_link(song_music_link);
+                song.setSong_lrc_link(song_lrc_link);
+                dao.insert(song);
+                a="，通过传入的歌曲参数已将歌曲插入数据库";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("插入歌曲（数据库）异常");
         }
 
         try {
-            int ex_user_id = Integer.valueOf(TokenControl.analysisToken(token));
+            int ex_user_id = 0;
+            try {
+                ex_user_id = Integer.valueOf(TokenControl.analysisToken(token));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                message.setBody(null);
+                message.setMessage("token出错"+a);
+                message.setStatus("3");
+                return message;
+            }
             if (!(dao.query(Playlist.class, Cnd.where("kt_user_id", "=", ex_user_id)).isEmpty())) {
                 message.setBody(null);
-                message.setMessage("排麦重复");
+                message.setMessage("排麦重复"+a);
                 message.setStatus("5");
                 return message;
             } else {
@@ -58,12 +84,9 @@ public class RoomUpDownMai {
                 playlist.setUser_id(ex_user_id);
 
                 dao.insert(playlist);
-                dao.insert(song);
-
                 message.setBody(null);
                 message.setMessage("success");
                 message.setStatus("1");
-
                 return message;
             }
         } catch (NumberFormatException e) {
@@ -79,17 +102,26 @@ public class RoomUpDownMai {
     /*
      * 下麦功能:根据token获取用户id，根据用户id删除Playlis表里的一条记录
      */
-    @At("/delete")
+    @At("/room/singer/delete")
     @Ok("Json")
     @DELETE
-    public Message roomDownM(@Param("token") String token) {
+    public Message roomDownM(@Param("token")String token) {
         Message message = new Message();
 
         try {
-            int ex_user_id = Integer.valueOf(TokenControl.analysisToken(token));
+            int ex_user_id = 0;
+            try {
+                ex_user_id = Integer.valueOf(TokenControl.analysisToken(token));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                message.setBody(null);
+                message.setMessage("失败：token解析出错");
+                message.setStatus("3");
+                return message;
+            }
             if (dao.query(Playlist.class, Cnd.where("kt_user_id", "=", ex_user_id)).isEmpty()) {
                 message.setBody(null);
-                message.setMessage("失败：不存在该房间");
+                message.setMessage("失败：不存在该用户");
                 message.setStatus("2");
                 return message;
             } else {
@@ -113,31 +145,48 @@ public class RoomUpDownMai {
     /*
      *  排麦功能：获取room_id,根据room_id（所有相同）在数据库的song表里加入song的各项信息，最后返回List<song>集合
      */
-    @At("/songs")
+    @At("/room/songs")
     @Ok("Json")
     @GET
-    public Message getSort(String room_id) {
+    public Message getSort(@Param("room_id") String room_id) {
 
         Message message = new Message();
         Song song = new Song();
         List<Song> songs = new ArrayList<Song>();
         List<Playlist> playlists = new ArrayList<Playlist>();
-
+        int ex_room_id = 0;
         try {
-            if (dao.query(Playlist.class, Cnd.where("kt_room_id", "=", room_id)).isEmpty()) {
+            ex_room_id = Integer.valueOf(room_id);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            message.setBody(null);
+            message.setMessage("失败：数据转换出错（String转int）");
+            message.setStatus("3");
+            return message;
+        }
+        try {
+            if (dao.query(Playlist.class, Cnd.where("kt_room_id", "=", ex_room_id)).isEmpty()) {
                 message.setBody(null);
                 message.setMessage("失败：不存在该房间");
                 message.setStatus("6");
                 return message;
             }
-            playlists = dao.query(Playlist.class, Cnd.where("kt_room_id", "=", room_id));
+            playlists = dao.query(Playlist.class, Cnd.where("kt_room_id", "=", ex_room_id));
             int song_id = 0;
+
             for (int i = 0; i < playlists.size(); i++) {
                 song_id = playlists.get(i).getSong_id();
                 song.setId(song_id);
-                songs.add(i, song);
+                System.out.println("song_id"+song_id);
+                songs.set(i,song);
             }
             message.setBody(songs);
+
+            for (Song s:
+                 songs) {
+                System.out.println("SONG_ID"+s.getId());
+            }
+
             message.setStatus("1");
             message.setMessage("success");
             return message;
